226140645ec42ab075e67a8254e74f9854f4055d
applenele <1941973283@qq.com>
2015-10-02 11:42:36 +0800

    1212

--- NW/NW/Controllers/UserController.cs
+++ NW/NW/Controllers/UserController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NW.BLL;
+using NW.Entity;
+using NW.Entity.DataModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,10 +12,31 @@ namespace NW.Controllers
     public class UserController : BaseController
     {
 
+        /// <summary>
+        /// 用户登录
+        /// </summary>
+        /// <param name="Username"></param>
+        /// <param name="Password"></param>
+        /// <returns></returns>
         [HttpPost]
         public ActionResult Login(string Username, string Password)
         {
-            return Content("ok");
+            AjaxModel model = new AjaxModel();
+            IBLL.IUserBLL bll = BLLSessionFactory.GetBLLSession().IUserBLL;
+            User user = new User();
+            user = bll.Login(Username, Password);
+            if (user == null)
+            {
+                model.Data = user;
+                model.Msg = "用户名或者密码不正确！";
+                model.Statu = "err";
+            }
+            else
+            {
+                model.Data = user;
+                model.Statu = "ok";
+            }
+            return Json(model);
         }
     }
 }
