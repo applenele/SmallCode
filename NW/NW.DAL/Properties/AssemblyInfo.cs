118d1382ca0dd6de376cb47d4d48b7066f5ede1c
applenele <1941973283@qq.com>
2015-10-02 11:14:14 +0800

    1212

--- NW/NW.DAL/Properties/AssemblyInfo.cs
+++ NW/NW.DAL/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("ＮＷ.DAL")]
+[assembly: AssemblyTitle("NW.DAL")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ＮＷ.DAL")]
+[assembly: AssemblyProduct("NW.DAL")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
