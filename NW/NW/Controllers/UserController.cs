ef62347d074abacdf05c6510cfb8bf2f2f3fcf46
applenele <1941973283@qq.com>
2015-10-02 12:50:28 +0800

    121212

--- NW/NW/Controllers/UserController.cs
+++ NW/NW/Controllers/UserController.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace NW.Controllers
 {
@@ -35,6 +36,7 @@ namespace NW.Controllers
             }
             else
             {
+                FormsAuthentication.SetAuthCookie(Username, false);
                 model.Data = user;
                 model.Statu = "ok";
             }
--- NW/NW/Scripts/codeqiaokeli.js
+++ NW/NW/Scripts/codeqiaokeli.js
@@ -4,7 +4,12 @@
         var username = $("#txtUsername").val();
         var password = $("#txtPassword").val();
         $.post("/User/Login", { username: username, password: password }, function (data) {
-            console.log(data);
+            if (data.Statu == "ok") {
+                window.location.reload();
+            }
+            else {
+
+            }
         });
     });
 });
