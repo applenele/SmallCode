b4704d4955f1a3724cfbe28736360a55c223608d
applenele <1941973283@qq.com>
2015-10-02 10:39:51 +0800

    util

---
+++ NW/NW.Ｕtility/DateHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NW.Utility
+{
+    public class DateHelper
+    {
+        #region 获取时间戳
+        /// <summary>  
+        /// 获取时间戳  
+        /// </summary>  
+        /// <returns></returns>  
+        public static string GetTimeStamp()
+        {
+            TimeSpan ts = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            return Convert.ToInt64(ts.TotalSeconds).ToString();
+        }
+        #endregion
+
+
+        /// <summary>
+        /// 两个时间之间的天数
+        /// </summary>
+        /// <param name="time1"></param>
+        /// <param name="time2"></param>
+        /// <returns></returns>
+        public static int DifferenceDays(DateTime time1, DateTime time2)
+        {
+            int ret = 0;
+            ret = (time1 - time2).Days;
+            return ret;
+        }
+    }
+}
---
+++ NW/NW.Ｕtility/Encryt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NW.Utility
+{
+    public class Encryt
+    {
+        /// <summary>
+        /// MD5加密
+        /// </summary>
+        /// <param name="sDataIn"></param>
+        /// <returns></returns>
+        public static string GetMD5(string sDataIn)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            byte[] bytValue, bytHash;
+            bytValue = System.Text.Encoding.UTF8.GetBytes(sDataIn);
+            bytHash = md5.ComputeHash(bytValue);
+            md5.Clear();
+            string sTemp = "";
+            for (int i = 0; i < bytHash.Length; i++)
+            {
+                sTemp += bytHash[i].ToString("X").PadLeft(2, '0');
+            }
+            return sTemp.ToLower();
+        }
+    }
+}
