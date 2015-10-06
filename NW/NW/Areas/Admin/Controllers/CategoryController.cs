aa45ab8293d876fb19b6cdd63449b48bb45a0f0a
applenele <1941973283@qq.com>
2015-10-06 15:44:20 +0800

    category

---
+++ NW/NW/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NW.Areas.Admin.Controllers
+{
+    public class CategoryController : BaseController
+    {
+        // GET: Admin/Category
+        public ActionResult Index(string Key)
+        {
+            string where = "";
+            if (!string.IsNullOrEmpty(Key))
+            {
+                where = where + "Description LIKE '%" + Key + "%'";
+            }
+            return View(bllSession.ICategoryBLL.GetList("").ToPagedList(1, 20));
+        }
+    }
+}
