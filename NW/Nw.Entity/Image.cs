b2227b44ba45bc0690b34930c25d0950b6c92e69
applenele <1941973283@qq.com>
2015-10-06 23:07:11 +0800

    image entity

---
+++ NW/Nw.Entity/Image.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NW.Entity
+{
+    public class Image
+    {
+        public int Id { set; get; }
+
+        public string Title { set; get; }
+
+        public string Description { set; get; }
+
+        public string Path { set; get; }
+
+        public DateTime Time { set; get; }
+    }
+}
