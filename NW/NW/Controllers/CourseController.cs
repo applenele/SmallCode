a513423b8772d2ff705dbeaccc880eca368dd8fd
applenele <1941973283@qq.com>
2015-10-07 21:56:54 +0800

    1212

---
+++ NW/NW/Controllers/CourseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NW.Controllers
+{
+    public class CourseController : BaseController
+    {
+        // GET: Course
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
