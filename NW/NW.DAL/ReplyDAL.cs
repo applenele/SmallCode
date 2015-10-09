055982e583a2614d1f56fb786cfeca22edeaafab
applenele <1941973283@qq.com>
2015-10-09 23:52:45 +0800

    1212

---
+++ NW/NW.DAL/ReplyDAL.cs
@@ -0,0 +1,80 @@
+﻿using NW.Entity;
+using NW.Factory;
+using NW.IDAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace NW.DAL
+{
+    public class ReplyDAL : IBaseDAL<Reply>, IReplyDAL
+    {
+
+        #region 得到数据库链接对象
+        private IDbConnection _conn;
+        public IDbConnection Conn
+        {
+            get
+            {
+                return _conn = ConnectionFactory.CreateConnection();
+            }
+        }
+        #endregion
+
+        public int Delete(int id)
+        {
+            using (Conn)
+            {
+                string query = "DELETE FROM Reply WHERE Id = @Id";
+                return Conn.Execute(query, new { Id = id });
+            }
+        }
+
+        public int Delete(Reply model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Reply GetEntity(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Reply GetEntityWithRefence(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Reply> GetList(string whereStr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Reply> GetListByPage(int page, int size, string whereStr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(Reply model)
+        {
+            using (Conn)
+            {
+                string query = "INSERT INTO Reply(UserId,Description,FatherId,Time)VALUES(@UserId,@Description,@FatherId,@Time)";
+                return Conn.Execute(query, model);
+            }
+        }
+
+        public int Update(Reply model)
+        {
+            using (Conn)
+            {
+                string query = "UPDATE Reply SET  UserId=@UserId,Description=@Description,FatherId=@FatherId,Time=@Time WHERE Id =@Id";
+                return Conn.Execute(query, model);
+            }
+        }
+    }
+}
---
+++ NW/Nw.Entity/Reply.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NW.Entity
+{
+    public class Reply
+    {
+        public int Id { set; get; }
+
+        public int UserId { set; get; }
+
+        public string Description { set; get; }
+
+        public int? FatherId { set; get; }
+
+        public DateTime Time { set; get; }
+    }
+}
