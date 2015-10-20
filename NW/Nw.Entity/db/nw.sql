4a9f94ab94ec383530ce1019a83a4eea8301001c
applenele <1941973283@qq.com>
2015-10-20 23:32:28 +0800

    db

---
+++ NW/Nw.Entity/db/nw.sql
@@ -0,0 +1,139 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localmysql
+Source Server Version : 50540
+Source Host           : localhost:3306
+Source Database       : nw
+
+Target Server Type    : MYSQL
+Target Server Version : 50540
+File Encoding         : 65001
+
+Date: 2015-10-20 23:31:17
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for article
+-- ----------------------------
+DROP TABLE IF EXISTS `article`;
+CREATE TABLE `article` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Title` varchar(255) DEFAULT NULL,
+  `Description` text,
+  `Time` datetime DEFAULT NULL,
+  `Browses` int(11) NOT NULL DEFAULT '0',
+  `Category` varchar(255) DEFAULT NULL,
+  `UserId` int(11) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for category
+-- ----------------------------
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Description` varchar(255) DEFAULT NULL,
+  `Time` datetime DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for course
+-- ----------------------------
+DROP TABLE IF EXISTS `course`;
+CREATE TABLE `course` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Title` varchar(255) DEFAULT NULL,
+  `Description` text,
+  `Time` datetime DEFAULT NULL,
+  `Category` varchar(255) DEFAULT NULL,
+  `Cover` varchar(255) DEFAULT NULL,
+  `Lecturer` varchar(255) DEFAULT NULL,
+  `Browses` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for image
+-- ----------------------------
+DROP TABLE IF EXISTS `image`;
+CREATE TABLE `image` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Title` varchar(255) DEFAULT NULL,
+  `Description` text,
+  `Path` varchar(255) DEFAULT NULL,
+  `Time` datetime DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for log
+-- ----------------------------
+DROP TABLE IF EXISTS `log`;
+CREATE TABLE `log` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Time` datetime DEFAULT NULL,
+  `Thread` varchar(255) DEFAULT NULL,
+  `Level` varchar(255) DEFAULT NULL,
+  `Type` varchar(255) DEFAULT NULL,
+  `Description` varchar(255) DEFAULT NULL,
+  `Exception` varchar(255) DEFAULT NULL,
+  `Ip` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for reply
+-- ----------------------------
+DROP TABLE IF EXISTS `reply`;
+CREATE TABLE `reply` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `UserId` int(11) NOT NULL,
+  `BlogId` int(11) NOT NULL,
+  `Description` text,
+  `FatherId` int(11) DEFAULT NULL,
+  `Time` datetime DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Username` varchar(255) DEFAULT NULL,
+  `Password` varchar(40) DEFAULT NULL,
+  `Email` varchar(40) DEFAULT NULL,
+  `Phone` varchar(20) DEFAULT NULL,
+  `QQ` varchar(20) DEFAULT NULL,
+  `Address` text,
+  `Remark` text,
+  `Program` varchar(255) DEFAULT NULL,
+  `Photo` varchar(255) DEFAULT NULL,
+  `RoleAsInt` int(1) DEFAULT NULL,
+  `Time` datetime DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for video
+-- ----------------------------
+DROP TABLE IF EXISTS `video`;
+CREATE TABLE `video` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `CourseId` int(11) NOT NULL,
+  `Title` varchar(255) DEFAULT NULL,
+  `Category` varchar(255) DEFAULT NULL,
+  `Path` varchar(255) DEFAULT NULL,
+  `Seret` varchar(255) DEFAULT NULL,
+  `Time` datetime DEFAULT NULL,
+  `Description` text,
+  `UserId` int(11) NOT NULL,
+  `AuthorityAsInt` int(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
