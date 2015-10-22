0f02b4c033088d4f5c4970391ea7c961bd304bfd
applenele <1941973283@qq.com>
2015-10-23 00:21:36 +0800

    1212

---
+++ NW/NW/Controllers/VideoController.cs
@@ -0,0 +1,31 @@
+﻿using NW.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NW.Controllers
+{
+    public class VideoController : BaseController
+    {
+        // GET: Video
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        ///  播放视屏
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Play(int id)
+        {
+            Video video = new Video();
+            video = bllSession.IVideoBLL.GetEntity(id);
+            return View(video);
+        }
+    }
+}
