a261a7aa6014388113126df8a00dbb94f2ba1479
YinYiPeng <18845296347@163.com>
2015-11-17 10:15:31 +0800

    Add_BBS_Entity

---
+++ NW/Nw.Entity/Replyforum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NW.Entity
+{
+    /// <summary>
+    /// 论坛回复表
+    /// </summary>
+    class Replyforum
+    {
+        public int Id { get; set; }
+
+        public int TopicId { get; set; }
+
+        public virtual Topicforum Topicforum { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public int UserId { get; set; }
+
+        public virtual User User { get; set; }
+
+        public int FatherId { get; set; }
+    }
+}
---
+++ NW/Nw.Entity/Topicforum.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NW.Entity
+{
+    /// <summary>
+    /// 论坛主题表
+    /// </summary>
+    class Topicforum
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public bool Top { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public DateTime LastReply { get; set; }
+
+        public int UserId { get; set; }
+
+        public virtual User User{ get; set; }
+
+        public int Reward { get; set; }
+
+        public int Browses { get; set; }
+
+        public int Report { get; set; }
+
+        public bool IsShow { get; set; }
+
+        public bool IsClose { get; set; }
+
+        public bool IsOfficeIdentified { get; set; }
+    }
+}
