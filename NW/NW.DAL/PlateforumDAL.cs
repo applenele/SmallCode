25f8f789cfe5c1ae82b03a7f815d6c0f7c527766
YinYiPeng <18845296347@163.com>
2015-11-19 10:35:04 +0800

    update PlateforumDAL

--- NW/NW.DAL/PlateforumDAL.cs
+++ NW/NW.DAL/PlateforumDAL.cs
@@ -13,19 +13,44 @@ namespace NW.DAL
 {
     public class PlateforumDAL : IBaseDAL<Plateforum>, IPlateforumDAL
     {
+        #region   得到数据库连接对象
+        private IDbConnection _conn;
+        public IDbConnection Conn
+        {
+            get
+            {
+                return _conn = ConnectionFactory.CreateConnection();
+            }
+        }
+        #endregion
+
         public int Delete(int id)
         {
-            throw new NotImplementedException();
+            using (Conn)
+            {
+                string query = "DELETE FROM Plateforum WHERE Id=@Id";
+                return Conn.Execute(query, new { Id = id });
+            }
         }
 
         public int Delete(Plateforum model)
         {
-            throw new NotImplementedException();
+            using (Conn)
+            {
+                string query = "DELETE FROM Plateforum WHERE Id=@Id";
+                return Conn.Execute(query, model);
+            }
         }
 
         public Plateforum GetEntity(int id)
         {
-            throw new NotImplementedException();
+            Plateforum plateforum;
+            string query = "SELECT * FROM Plateforum WHERE Id=@Id";
+            using (Conn)
+            {
+                plateforum = Conn.Query<Plateforum>(query, new { Id = id }).FirstOrDefault();
+                return plateforum;
+            }
         }
 
         public Plateforum GetEntityWithRefence(int id)
@@ -35,7 +60,19 @@ namespace NW.DAL
 
         public IEnumerable<Plateforum> GetList(string whereStr)
         {
-            throw new NotImplementedException();
+            using (Conn)
+            {
+                string query = "";
+                if (!string.IsNullOrEmpty(whereStr))
+                {
+                    query = "SELECT * FROM Plateforum where " + whereStr + "order by Time desc";
+                }
+                else
+                {
+                    query = "SELECT * FROM Plateforum order by Time desc";
+                }
+                return Conn.Query<Plateforum>(query);
+            }
         }
 
         public IEnumerable<Plateforum> GetListByPage(int page, int size, string whereStr)
@@ -45,12 +82,20 @@ namespace NW.DAL
 
         public int Insert(Plateforum model)
         {
-            throw new NotImplementedException();
+            using (Conn)
+            {
+                string query = "INSERT INTO Plateforum(Title,Picture,Description,Time)VALUES(@Title,@Picture,@Description,@Time)";
+                return Conn.Execute(query, model);
+            }
         }
 
         public int Update(Plateforum model)
         {
-            throw new NotImplementedException();
+            using (Conn)
+            {
+                string query = "UPDATE Plateforum SET Title=@Title,Picture=@Picture,Description=@Description,Time=@Time,Report=@Report,IsClose=@IsClose,Browses=@Browses WHERE Id=@Id";
+                return Conn.Execute(query, model);
+            }
         }
     }
 }
