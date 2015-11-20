5fca13c15a34cbf1e12b7df7eb63d69f31a31efe
YinYiPeng <18845296347@163.com>
2015-11-20 11:14:29 +0800

    update ReplyforumDAL

--- NW/NW.DAL/ReplyforumDAL.cs
+++ NW/NW.DAL/ReplyforumDAL.cs
@@ -23,7 +23,11 @@ namespace NW.DAL
         }
         public int Delete(int id)
         {
-            throw new NotImplementedException();
+            using (Conn)
+            {
+                string query = "DELETE FROM Replyforum WHERE Id=@Id";
+                return Conn.Execute(query, new { Id = id });
+            }
         }
 
         public int Delete(Replyforum model)
@@ -33,7 +37,12 @@ namespace NW.DAL
 
         public Replyforum GetEntity(int id)
         {
-            throw new NotImplementedException();
+            using (Conn)
+            {
+                string query = "SELECT * FROM Replyforum r join User u on r.UserId = u.Id WHERE Id=@Id";
+                var data = Conn.Query<Replyforum, User, Replyforum>(query, (replyforum, user) => { replyforum.User = user; return replyforum; }, new { Id = id });
+                return data.FirstOrDefault();
+            }
         }
 
         public Replyforum GetEntityWithRefence(int id)
@@ -43,7 +52,36 @@ namespace NW.DAL
 
         public IEnumerable<Replyforum> GetList(string whereStr)
         {
-            throw new NotImplementedException();
+            using (Conn)
+            {
+                string query = "";
+                if (string.IsNullOrEmpty(whereStr))
+                {
+                    query = "SELECT * FROM Replyforum r join User u on r.UserId = u.Id left join Replyforum rr on u.Id = rr.FatherId left join User uu on rr.UserId = uu.Id where r.FatherId is null order by r.Time desc ";
+                }
+                else
+                {
+                    query = "SELECT * FROM Replyforum r join User u on r.UserId = u.Id left join Replyforum rr on u.Id = rr.FatherId left join User uu on rr.UserId = uu.Id where r.FatherId is null and "+ whereStr +"order by r.Time desc";
+                }
+                Replyforum lookup = null;
+                var data = Conn.Query<Replyforum, User, Replyforum, User, Replyforum>(query,
+                    (replyforum, user, child, _user) =>
+                    {
+                        replyforum.User = user;
+                        if (lookup == null || lookup.Id != user.Id)
+                        {
+                            lookup = replyforum;
+                        }
+                        if (child != null)
+                        {
+                            child.User = _user;
+                            lookup.Children.Add(child);
+                        }
+                        return lookup;
+                    }
+                ).Distinct();
+                return data;
+            }
         }
 
         public IEnumerable<Replyforum> GetListByPage(int page, int size, string whereStr)
@@ -51,14 +89,56 @@ namespace NW.DAL
             throw new NotImplementedException();
         }
 
+        public IEnumerable<Replyforum> GetReplyforumAllFather(string whereStr)
+        {
+            using (Conn)
+            {
+                string query = "";
+                if (string.IsNullOrEmpty(whereStr))
+                {
+                    query = "SELECT * FROM Replyforum r left join User u on r.UserId = u.Id left join Replyforum rr on r.Id = rr.FatherId left join User uu on rr.UserId = uu.Id  order by r.Time desc";
+                }
+                else
+                {
+                    query = "SELECT * FROM Replyforum r left join User u on r.UserId = u.Id left join Replyforum rr on r.Id = rr.FatherId left join User uu on rr.UserId = uu.Id  where " + whereStr + " order by r.Time desc";
+                }
+                Replyforum lookup = null;
+                var data = Conn.Query<Replyforum, User, Replyforum, User, Replyforum>(query,
+                    (replyforum, user, child, _user) =>
+                    {
+                        replyforum.User = user;
+                        if (lookup == null || lookup.Id != replyforum.Id)
+                        {
+                            lookup = replyforum;
+                        }
+                        if (child != null)
+                        {
+                            child.User = _user;
+                            lookup.Children.Add(child);
+                        }
+                        return lookup;
+                    }).Distinct();
+
+                return data;
+            }
+        }
+
         public int Insert(Replyforum model)
         {
-            throw new NotImplementedException();
+            using (Conn)
+            {
+                string query = "INSERT INTO Replyforum(TopicId,Content,Time,UserId,FatherId)VALUES(@TopicId,@Content,@Time,@UserId,@FatherId)";
+                return Conn.Execute(query, model);
+            }
         }
 
         public int Update(Replyforum model)
         {
-            throw new NotImplementedException();
+            using (Conn)
+            {
+                string query = "UPDATE Replyforum SET TopicId=@TopicId,Content=@Content,Time=@Time,UserId=@UserId,FatherId=@FatherId";
+                return Conn.Execute(query, model);
+            }
         }
     }
 }
