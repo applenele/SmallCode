653c3cf26fde5293858c6657b66a3da9bce47bf1
YinYiPeng <18845296347@163.com>
2015-11-22 15:10:12 +0800

    update_Forum_UI_0.0.0.1

---
+++ NW/NW/Controllers/ForumController.cs
@@ -0,0 +1,20 @@
+﻿using NW.Entity;
+using NW.Entity.ViewModels;
+using NW.Utility;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NW.Controllers
+{
+    public class ForumController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
