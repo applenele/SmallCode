b4c2756ebcadee85aa3e396323a560220878ea7f
applenele <1941973283@qq.com>
2015-12-17 22:49:17 +0800

    blog  side

---
+++ NW/NW.Utility/SideHelper.cs
@@ -0,0 +1,57 @@
+﻿using NW.Entity.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NW.BLL;
+using NW.Entity;
+
+namespace NW.Utility
+{
+    public static class SideHelper
+    {
+        /// <summary>
+        ///  文章右边分类
+        /// </summary>
+        /// <returns></returns>
+        public static List<SideArticleCategory> GetSideCategoryCategories()
+        {
+            BLL.BLLSession bllSession = BLLSessionFactory.GetBLLSession();
+            List<Category> Categories = new List<Category>();
+            Categories = bllSession.ICategoryBLL.GetList("").ToList();
+            List<SideArticleCategory> Sides = new List<SideArticleCategory>();
+            foreach (var item in Categories)
+            {
+                SideArticleCategory side = new SideArticleCategory();
+                side.Id = item.Id;
+                side.Description = item.Description;
+                int count = bllSession.IArticleBLL.GetList("Category='" + item.Description + "'").Count();
+                side.Count = count;
+                Sides.Add(side);
+            }
+            return Sides;
+        }
+
+        /// <summary>
+        /// 按照日期查找
+        /// </summary>
+        /// <returns></returns>
+        public static List<SideArticleCalendar> GetSideArticleCalendars()
+        {
+            BLL.BLLSession bllSession = BLLSessionFactory.GetBLLSession();
+            List<SideArticleCalendar> Sides = new List<SideArticleCalendar>();
+
+            //分开写 容易懂
+            //List<DateTime> dates = bllSession.IArticleBLL.GetList("").Select(a => a.Time).ToList();
+            //Sides = dates.GroupBy(a => a.Date).Select(g =>(new SideArticleCalendar() {DateShow = g.Key,Count =  g.Count()})).ToList();
+
+            Sides = bllSession.IArticleBLL.GetList("")
+                .Select(a => a.Time)
+                .GroupBy(a => a.Date)
+                .Select(g => (new SideArticleCalendar() { DateShow = g.Key, Count = g.Count() }))
+                .ToList();
+            return Sides;
+        }
+    }
+}
---
+++ NW/Nw.Entity/ViewModels/SideArticleCalendar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NW.Entity.ViewModels
+{
+    /// <summary>
+    ///右边日历
+    /// </summary>
+    public class SideArticleCalendar
+    {
+        public DateTime DateShow { set; get; }
+
+        public int Count { set; get; }
+    }
+}
