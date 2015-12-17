4f5ebde3025a925663ef294620c6dc9d80c30c91
applenele <1941973283@qq.com>
2015-12-17 22:49:38 +0800

    attch

---
+++ NW/NW/Scripts/common.js
@@ -0,0 +1,13 @@
+﻿function popMsg(txt) {
+    var msg = $('<div class="msg hide">' + txt + '</div>');
+    msg.css('left', '50%');
+    $('body').append(msg);
+    msg.css('margin-left', '-' + parseInt(msg.outerWidth() / 2) + 'px');
+    msg.removeClass('hide');
+    setTimeout(function () {
+        msg.addClass('hide');
+        setTimeout(function () {
+            msg.remove();
+        }, 400);
+    }, 2600);
+}
